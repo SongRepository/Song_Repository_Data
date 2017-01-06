4a75f2d249fa79bb3974a0bb4b0030b988745111
Maijz128 <Maijz128@gmail.com>
2017-01-06 17:41:26 +0800

    initial commit

---
+++ data-manager.js
@@ -0,0 +1,86 @@
+var _Path = require('path');
+var _FileSystem = require('./../Song_Repository_Server/src/file-system');
+
+
+var CollectionRoot = __dirname + '/data/collection/';
+var IndexRoot = __dirname + '/data/index/';
+
+
+var RegisterManager = function () {
+
+};
+RegisterManager.prototype.getFileBaseName = function (fileName) {
+    return _Path.basename(fileName, _Path.extname(fileName));
+};
+RegisterManager.prototype.getCollectionName = function (jsonObj) {
+    return jsonObj['name'];
+};
+RegisterManager.prototype.isCollectionFile = function (fileName) {
+    const JSON = '.json';
+    return _Path.extname(fileName) === JSON;
+};
+RegisterManager.prototype.buildCollectionItem = function (fileBaseName, name) {
+    var item = {
+        file_name: fileBaseName,
+        name: name
+    };
+    return item;
+};
+RegisterManager.prototype.readCollectionFileList = function () {
+    return _FileSystem.readDir(CollectionRoot);
+};
+RegisterManager.prototype.readCollectionFile = function (fileName) {
+    var filePath = _Path.join(CollectionRoot, fileName);
+    return _FileSystem.readJsonFile(filePath);
+};
+RegisterManager.prototype.saveRegister = function (register) {
+    var registerFileName = 'register.json';
+    var filePath = _Path.join(IndexRoot, registerFileName);
+    _FileSystem.saveObjToFile(register, filePath);
+};
+RegisterManager.prototype.buildRegister = function (fileList) {
+    var self = this;
+    var result = [];
+    try {
+        fileList.forEach(function (fileName) {
+            if (self.isCollectionFile(fileName)) {
+                var fileBaseName = self.getFileBaseName(fileName);
+                var jsonObj = self.readCollectionFile(fileName);
+                var name = self.getCollectionName(jsonObj);
+                var item = self.buildCollectionItem(fileBaseName, name);
+                result.push(item);
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+    return result;
+};
+RegisterManager.prototype.autoBuildRegister = function () {
+    var self = this;
+    var fileList = self.readCollectionFileList();
+    var register = self.buildRegister(fileList);
+    if (register) {
+        self.saveRegister(register);
+    } else {
+        console.error('RegisterManager.autoBuildRegister() is failed.');
+    }
+};
+
+
+var DataManager = function () {
+    var self = this;
+
+    self._RegisterManager = new RegisterManager();
+    setTimeout(function () {
+        self._RegisterManager.autoBuildRegister();
+    }, 1);
+
+    return self;
+};
+
+
+var dataManager = new DataManager();
+
+module.exports = exports = dataManager;
---
+++ data-manager.test.js
@@ -0,0 +1,69 @@
+var _DataManager = require('./data-manager');
+var _Assert = require("assert");
+
+var _RegisterManager = _DataManager._RegisterManager;
+
+describe("RegisterManager", function () {
+
+    describe('getFileBaseName', function () {
+        it('', function () {
+            var fileName = 'aaabbb.json';
+            var fileName2 = 'aaa/bbb/ccc.exe';
+
+            var result = _RegisterManager.getFileBaseName(fileName);
+            var result2 = _RegisterManager.getFileBaseName(fileName2);
+
+            _Assert.equal(result, 'aaabbb');
+            _Assert.equal(result2, 'ccc');
+        });
+    });
+
+    describe('getCollectionName', function () {
+        it('', function () {
+            var jsonObj = {
+                name: 'name'
+            };
+
+            var result = _RegisterManager.getCollectionName(jsonObj);
+
+            _Assert.equal(result, 'name');
+        });
+    });
+
+    describe('isCollectionFile', function () {
+        it('', function () {
+            var fileName = 'aaabbb.json';
+            var fileName2 = 'adddd.js';
+
+            var result = _RegisterManager.isCollectionFile(fileName);
+            var result2 = _RegisterManager.isCollectionFile(fileName2);
+
+            _Assert.equal(result, true);
+            _Assert.equal(result2, false);
+
+        });
+    });
+
+    describe('buildCollectionItem', function () {
+        it('', function () {
+            var fileBaseName = 'honey-and-clover';
+            var name = '蜂蜜与四叶草';
+
+            var result = _RegisterManager.buildCollectionItem(fileBaseName, name);
+
+            _Assert.equal(result['file_name'], fileBaseName);
+            _Assert.equal(result['name'], name);
+
+        });
+    });
+
+    describe('buildRegister', function () {
+        it('', function () {
+
+            var fileList = _RegisterManager.readCollectionFileList();
+            var result = _RegisterManager.buildRegister(fileList);
+
+            //_Assert.equal(result, {});
+        });
+    });
+});
